f010859d34f1574a716c69b1a43043188595b05b
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2024-11-18 19:25:10 +0100

    add all files

--- Javascript1-week2/event-application.js
+++ Javascript1-week2/event-application.js
@@ -7,9 +7,5 @@ function getEventWeekday (daysToEvent){
   const eventDayNumber = (dayNumber + daysToEvent) % 7;
   return days[eventDayNumber];
 }
+
 console.log(getEventWeekday(5));
-//more practice :)
-/*
-const daysToEvent = prompt('Enter the number of days until the event:','')
-console.log(getEventWeekday(daysToEvent));
-*/
